c2a552043207789ffa2cb0a3c666e042363c2eec
Juditsatu <100708807+Juditsatu@users.noreply.github.com>
2022-06-14 12:59:59 +0200

    first commit

---
+++ data.ts
@@ -0,0 +1,16 @@
+const jokesUrl = "https://icanhazdadjoke.com/";
+let options = { 
+    headers: {
+        "Accept" : "application/json" 
+    }
+};
+
+async function generateJoke() {
+    const dataJokes:any = await fetch(jokesUrl, options);
+    const data:any = await dataJokes.json();
+    console.log(data);
+};
+
+function nextJoke() {
+    return console.log(generateJoke());
+}
